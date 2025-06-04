b315b1554da9e189c34be548c975d6ae54081c8f
jeckox <jeckox@gmail.com>
2025-06-04 17:25:18 -0600

    Implement logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,56 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useMemo} from 'react';
 import './App.css';
 
 function App() {
-  const wordToGuess = "banana";
+  const wordToGuess = "banana".toUpperCase();
   const [value, setValue] = useState("");
   const [attempts, setAttempts] = useState(0);
-  const [lettersToGuessed, setLettersToGuessed] = useState<Map<string, boolean>>(new Map());
+  const [lettersToGuessed, setLettersToGuessed] = useState(new Map());
 
   const initGame = () => {
     setAttempts(0);
     const letters = wordToGuess.split("");
     const temporaryLettersToGuessed = new Map();
-    for (const letter of letters) {
+    letters.forEach((letter) => {
       temporaryLettersToGuessed.set(letter, false);
-    }
+    });
     setLettersToGuessed(temporaryLettersToGuessed);
-    console.log(lettersToGuessed);
   }
   useEffect(() => {
     initGame();
   }, []);
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    setValue(event.key);
+    const newValue = event.key.toUpperCase();
+    setValue(newValue);
     setAttempts((prev) => prev + 1);
-    console.log(lettersToGuessed.entries());
+    const newLettersToGuessed = new Map(lettersToGuessed);
+    if (lettersToGuessed.has(newValue)) {
+      newLettersToGuessed.set(newValue, true);
+      setLettersToGuessed(newLettersToGuessed);
+    }
   }
+
+  const currentStatus = useMemo(() => {
+    const isWon = Array.from(lettersToGuessed.values()).every((value) => value === true);
+    const isLost = attempts >= 6 && !isWon;
+    return isLost ? "lost" : isWon ? "won" : "playing";
+  }, [lettersToGuessed, attempts]);
+  
+  
   return (
     <div className="App">
-        <div className="word-container">
-          <p>{ attempts }</p>
-          <p>{ lettersToGuessed.entries() }</p>
-        </div>
-      <input type="text" value={value} onKeyDown={handleKeyDown} maxLength={1}/>
+      <div className="game-container">
+          <div className="word-container">
+            {wordToGuess.split("").map((l) => <><span>{lettersToGuessed.get(l) ? l : "_"}</span><span>{" "}</span></>)}
+          </div>
+          <div className="input-container">
+            <input type="text" value={value} onKeyDown={handleKeyDown} maxLength={1} disabled={currentStatus !== "playing"} />
+          </div>
+          <p>Attempts: { attempts }</p>
+      </div>
+      <div className="message-container">
+        <p>You {currentStatus === "won" ? "won" : currentStatus === "lost" ? "lost" : "are playing  "}</p>
+      </div>
     </div>
   );
 }
